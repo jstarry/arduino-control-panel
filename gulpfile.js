b6bb58835caf437cbe2519c35980c78c85731352
Justin Starry <jstarry@users.noreply.github.com>
2018-01-13 19:35:13 -0500

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+'use strict';
 
-gulp.task('browserify', function() {
-  return gulp.src(['./index.js'])
-    .pipe(browserify({
-      debug : true,
-      "fullPaths": true
-    }))
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+gulp.task('browserify', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './index.js',
+    debug: true,
+  });
+
+  return b.bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
     .pipe(gulp.dest('./build/'));
 });
+
+
 
 gulp.task('watch', function() {
   gulp.watch(['./**/*.js', './*.js'], ['build']);
